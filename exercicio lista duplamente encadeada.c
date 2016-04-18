37ead5d22a5a249dfb8b3d56b23a3b5f17b50b2e
Filipe <filipeduarte.si@gmail.com>
2016-04-18 16:53:52 -0700

    aula do dia 18 listas duplamente encadeadas

--- exercicio lista duplamente encadeada.c
+++ exercicio lista duplamente encadeada.c
@@ -13,4 +13,43 @@ Pesquise sobre aplicações de listas duplamente encadeadas
 
 */
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+struct duplamente{
+	int info;
+	struct duplamente *ant;
+	struct duplamente *prox;
+};
+typedef struct duplamente Lista;
+
+
+Lista *inserir(Lista *l, int v){
+	Lista *novo = (Lista*) malloc (sizeof(Lista));
+		novo->info = v;
+		novo->prox = lista;
+		novo->ant = NULL;
+			if ( lista != NULL) {
+				lista ->ant = novo;
+			}
+	return novo;
+
+}
+
+main ()
+{
+	Lista *L;
+	L = NULL;
+	int valor;
+	
+	printf("informe um valor: \n");
+	scanf("%d", &valor);	
+	L = insere (L, valor);
+	
+	
+	
+system("pause");
+}
+
 
